10f60ef54e58cfc7f7f0fcb390707c264f9e4775
BrayanGuerreroXD <bg7047653@gmail.com>
2022-10-07 10:47:58 -0500

    Se han creado los html del proyecto

---
+++ js/index.js
@@ -0,0 +1,5 @@
+function getData() {
+    fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
+        .then((response) => response.json())
+        .then((data) => fillTable(data));
+}
